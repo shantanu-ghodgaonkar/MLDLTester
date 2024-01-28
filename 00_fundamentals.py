534d514fe300136d274769b3e9aa09b6a98008b0
Shantanu Ghodgaonkar <shantanu.ghodgaonkar@gmail.com>
2024-01-27 20:22:38 -0500

    Reshaping, viewing, stacking, squeezing, unsqueezing and permuting
    Explored the usage of reshaping, viewing, stacking, squeezing, unsqueezing and permuting

--- 00_fundamentals.py
+++ 00_fundamentals.py
@@ -88,12 +88,54 @@ import matplotlib.pyplot as plt
 # tensor_B = torch.rand(3,2)
 # print(torch.mm(tensor_A.T, tensor_B))
 
-x = torch.arange(0, 100, 10)
+# x = torch.arange(0, 100, 10)
 
-print(x)
-# print(f"Min = {x.min()}")
-# print(f"Max = {x.max()}")
-# print(f"Mean = {x.type(torch.float32).mean()}")
-# print(f"Median = {x.type(torch.float32).median()}")
-# print(f"Median = {x.type(torch.float32).mode()}")
-# print(f"Sum of Elements = {x.sum()}")
+# print(x)
+# # print(f"Min = {x.min()}")
+# # print(f"Max = {x.max()}")
+# # print(f"Mean = {x.type(torch.float32).mean()}")
+# # print(f"Median = {x.type(torch.float32).median()}")
+# # print(f"Median = {x.type(torch.float32).mode()}")
+# # print(f"Sum of Elements = {x.sum()}")
+
+# print(f"Index location of x.min() value = {x.argmin()}")
+# print(f"Index location of x.max() value = {x.argmax()}")
+
+# x = torch.arange(1., 10.)
+# print(f"x = {x}")
+# print(f"x.shape = {x.shape}")
+
+# x_reshaped = x.reshape(3,1,3)
+# print(f"x_reshaped = {x_reshaped}")
+# print(f"x_reshaped.shape = {x_reshaped.shape}")
+
+# z = x.view(3,3)
+# print(f"z = {z}")
+# print(f"z.shape = {z.shape}")
+
+# NOTE: Changing z changes x because a view of a tensor shares the same memory location as the original tensor
+
+# x_stacked = torch.stack([x,x,x,x],dim=0)
+# print(f"x_stacked = {x_stacked}")
+# print(f"x_stacked.shape = {x_stacked.shape}")
+
+# NOTE: See torch.vstack and torch.hstack
+
+# x_squeeze = x_reshaped.squeeze()
+# print(f"x_squeeze = {x_squeeze}")
+# print(f"x_squeeze.shape = {x_squeeze.shape}")
+
+# x_unsqueeze = x_squeeze.unsqueeze(dim=0)
+# print(f"x_unsqueeze = {x_unsqueeze}")
+# print(f"x_unsqueeze.shape = {x_unsqueeze.shape}")
+
+# x = torch.rand(size=(224,224,3)) # [height, width, colour channel]
+# print(f"x = {x}")
+# print(f"x.shape = {x.shape}")
+
+# x_permute = x.permute(2,0,1)
+# print(f"x_permute = {x_permute}") # [colour channel, height, width]
+# print(f"x_permute.shape = {x_permute.shape}")
+# # NOTE: torch.permute also returns a view, so be careful
+
+
